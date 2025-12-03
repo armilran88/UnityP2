f8e1f3ebdbda16fe5a160e0601a91de3a67449b7
armilran88 <armilran88@gmail.com>
2025-12-03 17:18:34 +0900

    미니맵 카메라 컬링마스크

--- Assets/Scripts/CamFollowTarget.cs
+++ Assets/Scripts/CamFollowTarget.cs
@@ -4,19 +4,20 @@ public class CamFollowTarget : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    // Update is called once per frame
     void Update()
     {
         // 미니맵 카메라가 플레이어 따라다니기
         FollowTarget();
-
     }
 
     void FollowTarget()
     {
         if (target == null) return;
-        // 타겟(플레이어)이 이동하는 X, Z값만 있으면된다.
+        // 타겟(플레이어)이 이동하는 X, Z값만 있으면 된다
         // 높이값은 본인 자신의 값 그대로 사용하자
-        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.position = new Vector3(
+            target.position.x,
+            transform.position.y,
+            target.position.z);
     }
 }
